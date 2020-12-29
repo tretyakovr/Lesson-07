343874ec6eed30fd17cc2bd2716df26d18fbe40e
rtretyakov <rtretyakov@outlook.com>
2020-12-30 00:30:36 +1000

    All tasks completed

--- Task-02.py
+++ Task-02.py
@@ -12,3 +12,47 @@
 # Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке
 # знания: реализовать абстрактные классы для основных классов проекта, проверить на практике
 # работу декоратора @property.
+
+from abc import ABC, abstractmethod
+
+class clothes(ABC):
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_cloth_count(self):
+        pass
+
+class coat(clothes):
+
+    def __init__(self, v):
+        self.v = v
+
+    @property
+    def get_cloth_count(self):
+        return self.v/6.5 + 0.5
+
+class suit(clothes):
+
+    def __init__(self, h):
+        self.h = h
+
+    @property
+    def get_cloth_count(self):
+        return self.h * 2 + 0.3
+
+
+c1 = coat(52)
+print(c1.get_cloth_count)
+
+c2 = coat(56)
+print(c2.get_cloth_count)
+
+
+s1 = suit(164)
+print(s1.get_cloth_count)
+
+s2 = suit(172)
+print(s2.get_cloth_count)
--- Task-03.py
+++ Task-03.py
@@ -34,3 +34,37 @@
 # вернет строку: *****\n*****\n*****.
 #
 # Подсказка: подробный список операторов для перегрузки доступен по ссылке.
+
+class cell():
+
+    def __init__(self, cell_count):
+        self.cell_count = cell_count
+
+    def __add__(self, other):
+        return self.cell_count + other.cell_count
+
+    def __sub__(self, other):
+        ret_val = self.cell_count - other.cell_count
+        return  ret_val if ret_val > 0 else 'error'
+
+    def __mul__(self, other):
+        return self.cell_count * other.cell_count
+
+    def __truediv__(self, other):
+        return self.cell_count // other.cell_count
+
+    def make_order(self, row_cells):
+        self.row_cells = row_cells
+
+        return ('*' * self.row_cells + '\n') * (self.cell_count //self.row_cells) + '*' * (self.cell_count % self.row_cells)
+
+
+c1 = cell(100)
+c2 = cell(6)
+
+print(c1 + c2)
+print(c1 - c2)
+print(c1 * c2)
+print(c1 / c2)
+
+print(c1.make_order(15))
